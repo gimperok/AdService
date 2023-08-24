892af6132b995ea347a9ca924b222a809def6b72
kozhevnikov <kozhevnikov@a-group.aero>
2023-08-24 17:22:32 +0300

    #3 - Добавлено описание методов API в сваггере;

--- Program.cs
+++ Program.cs
@@ -12,7 +12,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var basePath = AppContext.BaseDirectory;
+
+    var xmlPath = Path.Combine(basePath, "swaggerConfig.xml");
+    options.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlite(AppSettings.ConnectionString));
 
